ab7b20b14d8ce08369c3c133576c912bb8cfa27b
Henning Jacobs <henning@jacobs1.de>
2014-11-13 19:26:08 +0100

    running docker container (nginx on port 80) on a new EC2 instance

--- stuff.py
+++ stuff.py
@@ -45,13 +45,58 @@ key = conn.create_key_pair(key_name)
 key_dir = os.path.expanduser('~/.ssh')
 key.save(key_dir)
 sg = conn.create_security_group(sg_name, 'Some test group created by hjacobs', vpc_id='vpc-c649acaf')
-modify_sg(conn, sg, SecurityGroupRule("tcp", 22, 22, "0.0.0.0/0", None), authorize=True)
+
+rules = [
+    SecurityGroupRule("tcp", 22, 22, "0.0.0.0/0", None),
+    SecurityGroupRule("tcp", 80, 80, "0.0.0.0/0", None),
+]
+
+for rule in rules:
+    modify_sg(conn, sg, rule, authorize=True)
 
 interface = boto.ec2.networkinterface.NetworkInterfaceSpecification(subnet_id='subnet-72babe0a',
                                                                             groups=[sg.id],                                                                                                                                                associate_public_ip_address=True)
 interfaces = boto.ec2.networkinterface.NetworkInterfaceCollection(interface)
 
-reservation = conn.run_instances('ami-b83c0aa5', instance_type='t2.micro', network_interfaces=interfaces, key_name=key_name)
+init_script = b'''#!/bin/bash
+
+# use the latest HAProxy with IPv6 support
+add-apt-repository -y ppa:vbernat/haproxy-1.5
+
+# add Docker repo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+echo 'deb https://get.docker.io/ubuntu docker main' > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+
+#vim editor (only temporarily for test setup)
+apt-get install vim -y
+
+# system utilities (only temporarily for test setup)
+apt-get install -y strace tcpdump
+
+# HAProxy
+mkdir -p /fakepath
+ln -fs /bin/true /fakepath/invoke-rc.d
+PATH=/fakepath:$PATH apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y haproxy
+
+# Docker
+apt-get install -y --no-install-recommends -o Dpkg::Options::="--force-confold" apparmor lxc-docker
+
+# brctl
+apt-get install -y --no-install-recommends bridge-utils
+
+# Python
+apt-get install -y --no-install-recommends python3-all python3-pip python3-dev python3-setuptools
+apt-get install -y --no-install-recommends gcc build-essential
+
+# stupid workaround..
+pip3 install netifaces==0.10.4
+
+docker run -d -p 80:80 nginx
+'''
+
+reservation = conn.run_instances('ami-b83c0aa5', instance_type='t2.micro', network_interfaces=interfaces, key_name=key_name, user_data=init_script)
 instance = reservation.instances[0]
 instance.add_tags({'Name': 'hjacobs-test-stuff'})
 
@@ -62,7 +107,7 @@ while status == 'pending':
     status = instance.update()
 
 print('Instance status: ' + status)
-print(instance.public_dns_name)
+print(instance.id)
 
 
 key_extension = '.pem'
@@ -71,8 +116,14 @@ key_path = os.path.join(os.path.expanduser(key_dir),
                                 key_name+key_extension)
 s = FakeServer(instance, key_path)
 # HACK: we do not have public DNS...
-s.hostname = instance.ip_address
-print(s.ssh_key_file)
+while True:
+    if instance.ip_address and len(instance.ip_address) > 4:
+        s.hostname = instance.ip_address
+        break
+    print('Waiting for IP address..')
+    instance = conn.get_only_instances(instance_ids=[instance.id])[0]
+    time.sleep(3)
+print('ssh -i {} ubuntu@{}'.format(s.ssh_key_file, s.hostname))
 cmd = SSHClient(s, os.path.expanduser('~/.ssh/known_hosts'), login_user)
 res = cmd.run('uptime')
 print(res)
