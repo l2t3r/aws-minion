f9fe64ed31fa49f44d84e36692cf33fbf8c96e56
Henning Jacobs <henning@jacobs1.de>
2014-12-10 13:57:31 +0100

    remove VERSION marker file

--- release.sh
+++ release.sh
@@ -14,11 +14,12 @@ version=$1
 
 sed -i "s/__version__ = .*/__version__ = '${version}'/" aws_minion/__init__.py
 git add aws_minion/__init__.py
-
-printf ${version} > VERSION
-git add VERSION
+sed -i "s/version = .*/version = '${version}'/" docs/conf.py
+sed -i "s/release = .*/release = '${version}'/" docs/conf.py
+git add docs/conf.py
 
 git commit -m "Bumped version to $version"
+git push
 
 python3 setup.py clean
 python3 setup.py test
@@ -26,4 +27,4 @@ python3 setup.py test
 python3 setup.py sdist upload
 
 git tag ${version}
-git push --follow-tags
+git push --tags
